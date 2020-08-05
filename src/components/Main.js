fa3c0db22dcbca65010800e3ffc0276a07cb7a74
Dario Campagna <dario@localhost.localdomain>
2020-08-05 11:28:04 -0300

    Função para editar tarefa

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,18 +13,29 @@ export default class Main extends Component{
     state = {
         novaTarefa: '',
         tarefas: [],
+        index: -1,
     };
 
     handleSubmit = (e) => {
         e.preventDefault();
-        const { tarefas } = this.state;
+        const { tarefas, index } = this.state;
         let { novaTarefa } = this.state;
         novaTarefa = novaTarefa.trim();
         if(tarefas.indexOf(novaTarefa) !== -1) return;
         const novasTarefas = [...tarefas];
+
+        if (index === -1){
         this.setState({
             tarefas: [...novasTarefas, novaTarefa],
+            novaTarefa: '',
         });
+        }else{
+            novasTarefas[index] = novaTarefa;
+            this.setState({
+                tarefas: [...novasTarefas],
+                index: -1,
+            });
+        }
     }
 
     handleChange = (e) => {
@@ -34,7 +45,11 @@ export default class Main extends Component{
     }
 
     handleEdit = (e, index) => {
-
+        const { tarefas } = this.state;
+        this.setState({
+            index,
+            novaTarefa: tarefas[index],
+        });
     }
 
     handleDelete = (e, index) => {
